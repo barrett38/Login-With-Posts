29add3b1e6777de1f7e7dba931d5aad5c2ea2246
Barrett Anderson <barrett1614@gmail.com>
2024-10-21 12:59:00 -0500

    Added README

---
+++ README.md
@@ -0,0 +1,10 @@
+Installs:
+`npm install
+`npm install dotenv
+
+Front End Start:
+`npm start
+
+Back End Start:
+`cd server
+`nodemon index.js
--- server/util/database.js
+++ server/util/database.js
@@ -1,17 +1,18 @@
-require('dotenv').config()
-const {CONNECTION_STRING} = process.env
-const Sequelize = require('sequelize')
+require("dotenv").config();
+const CONNECTION_STRING = "INSERT YOUR CONNECTION STRING HERE";
+console.log("CONNECTION_STRING:", CONNECTION_STRING);
+const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(CONNECTION_STRING, {
-    dialect: 'postgres',
-    // dialectOptions: {
-    //     ssl: {
-    //         require: true,
-    //         rejectUnauthorized: false
-    //     }
-    // }
-})
+  dialect: "postgres",
+  // dialectOptions: {
+  //     ssl: {
+  //         require: true,
+  //         rejectUnauthorized: false
+  //     }
+  // }
+});
 
 module.exports = {
-    sequelize
-}
+  sequelize,
+};
